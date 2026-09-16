0f5c0bc2e927411461671152b769e8073f65e849
gopssgopika38-commits <gopssgopika38@gmail.com>
2026-09-16 12:10:14 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,11 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a = 10
+... b = 3
+... 
+... print("Addition:", a + b)
+... print("Subtraction:", a - b)
+... print("Multiplication:", a * b)
+... print("Division:", a / b)
+... print("Floor Division:", a // b)
+... print("Modulus:", a % b)
